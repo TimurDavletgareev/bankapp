73672ad9a5d58b0de25ff583d39fe810352624f6
TimurDV88 <kumba-yo@yandex.ru>
2025-08-20 11:42:23 +0500

    fix notification backoff policy

--- notification/src/main/java/ru/yandex/practicum/kafka/NotificationConsumer.java
+++ notification/src/main/java/ru/yandex/practicum/kafka/NotificationConsumer.java
@@ -6,7 +6,6 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.dto.NotificationDto;
 import ru.yandex.practicum.service.NotificationService;
@@ -20,11 +19,7 @@ public class NotificationConsumer {
 
     private final NotificationService notificationService;
 
-    @RetryableTopic(
-            attempts = "5", // Максимальное количество попыток
-            backoff = @Backoff(delay = 1000, multiplier = 2, maxDelay = 5000), // Экспоненциальная задержка
-            dltTopicSuffix = "-dlt" // Суффикс для DLT
-    )
+    @RetryableTopic
     @KafkaListener(topics = "${topic.notification}")
     public void onMessage(@Header(RECEIVED_TOPIC) String receivedTopic,
                           @Payload NotificationDto notificationDto) {
